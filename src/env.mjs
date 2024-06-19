250ee83587f6f298c255449261f9e70ff252e7a2
ray <me@rayhanadev.com>
2024-06-19 12:52:10 -0500

    chore: setup configs, shadcn, env, firebase

---
+++ src/env.mjs
@@ -0,0 +1,22 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
+  client: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string(),
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string(),
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string(),
+    NEXT_PUBLIC_FIREBASE_APP_ID: z.string(),
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: z.string(),
+  },
+  runtimeEnv: process.env,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = new URL(request.url);
+
+  const response = NextResponse.next();
+
+  response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
+  response.headers.set("Cross-Origin-Resource-Policy", "same-origin");
+  response.headers.set("Origin-Agent-Cluster", "?1");
+  response.headers.set(
+    "Strict-Transport-Security",
+    "max-age=15552000; includeSubDomains"
+  );
+  response.headers.set("X-Content-Type-Options", "nosniff");
+  response.headers.set("X-DNS-Prefetch-Control", "off");
+  response.headers.set("X-Download-Options", "noopen");
+  response.headers.set("X-Frame-Option", "SAMEORIGIN");
+  response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
+  response.headers.set("X-XSS-Protection", "0");
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    {
+      source: "/((?!_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
